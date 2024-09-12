d976453de4d6309a1fee6ad3eafef61b56b81f0e
abdalrhman <radea4082@gmail.com>
2024-09-12 10:28:56 +0300

    updat server

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const http = require("node:http")
-
 const server = http.createServer((req,res)=>{
-    res.end();
+    res.end("hello every body can do it");
 })
-
-server.listen(3001,()=>{
+ server.listen(3001,()=>{
     console.log("listening on port 3001")
 })
-
 
 
 
